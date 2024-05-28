4be56a013772d3cbb8e8c172d6f9963f09ae6a89
Swadha-Sri <swadhasri607@gmail.com>
2024-05-28 23:58:54 +0530

    Initial commit

--- in.php
+++ in.php
@@ -1,58 +1,57 @@
-<?php
-    include('db.php');
 
-    if(isset($_POST['Sign Up']))
-    {
-        $Username = $_POST['Username'];
-        $EmailAddress = $_POST['EmailAddress'];
-        $Password = $_POST['Password'];
-        $ConfirmPassword = $_POST['ConfirmPassword'];
+<?php 
 
-        if(!empty($EmailAddress) && !empty($Password) && !empty($ConfirmPassword)  && !is_numeric($EmailAddress))
-        {
-            $query = "INSERT INTO form (Username, EmailAddress, Password, ConfirmPassword)  VALUES ('$Username','$EmailAddress','$Password','$ConfirmPassword')";
+include 'db.php';
 
-            if($conn->query($query)==TRUE)
-            {
-                header("location: db.php");
-            }
-            else{
-                echo "Error: ".$conn->error;
-            }
-        }
-        else
-        {
-            echo "<script type='text/javascript'> alert('Unsuccessfull')</script>";
-        }
-    }
+if(isset($_POST['signUp']))
+{
+    $firstName=$_POST['fName'];
+    $lastName=$_POST['lName'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password=md5($password);
 
-    if(isset($_POST['Login']))
-    {
-        $Username = $_POSS['Username'];
-        $Password = $_POSS['Password'];
-
-        if(!empty($Username) && !empty($Password)  && !is_numeric($Username))
-        {
-
-            $sqlquery = "SELECT * FROM form WHERE EmailAddress = '$Username' limit 1";
-            $result = $conn->query($sqlquery);
-
-            if($result->num_rows > 0)
-            {
-                session_start();
-                $row=$result->fetch_assoc();
-                $_SESSION['Username']=$row['Username'];
-                header("Location: home.php");
-                exit(); 
+     $checkEmail="SELECT * From users where email='$email'";
+     $result=$conn->query($checkEmail);
+     if($result->num_rows>0)
+     {
+        echo "Email Address Already Exists !";
+     }
+     else
+     {
+        $insertQuery="INSERT INTO users(firstName,lastName,email,password) VALUES ('$firstName','$lastName','$email','$password')";
+            if($conn->query($insertQuery)==TRUE){
+                header("location: pro2.php");
             }
             else
             {
-                echo "Not Found, Incoreect Email or Password";
+                echo "Error:".$conn->error;
             }
-        }
-        else
-        {
-            echo "<script type='text/javascript'> alert('Unsuccessfull')</script>";
-        }
-    }
+     }
+   
+
+}
+
+if(isset($_POST['signIn']))
+{
+   $email=$_POST['email'];
+   $password=$_POST['password'];
+   $password=md5($password) ;
+   
+   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+   $result=$conn->query($sql);
+   if($result->num_rows>0)
+   {
+    session_start();
+    $row=$result->fetch_assoc();
+    $_SESSION['email']=$row['email'];
+    header("Location: home.php");
+    exit();
+   }
+   else
+   {
+    echo "Not Found, Incorrect Email or Password";
+   }
+
+}
 ?>
--- pro2.js
+++ pro2.js
@@ -1,54 +1,15 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+const signUpButton=document.getElementById('signUpButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const signUpForm=document.getElementById('signup');
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-
-        // Perform your AJAX/Fetch login
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 8) {
-                setInputError(inputElement, "Username must be at least 8 characters in length");
-            }
-        });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
+signUpButton.addEventListener('click',function()
+{
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function()
+{
+    signInForm.style.display="block";
+    signUpForm.style.display="none";
+})
